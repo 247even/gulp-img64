719cbdcc0579d024df88c847a5b82b45c3b56e38
247even <shoot.my.image@gmail.com>
2014-03-05 17:38:12 +0100

    Convert and replace image-files within your DOM/HTML to base64-encoded data.
    Convert and replace image-files within your DOM/HTML to base64-encoded
    data.

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+var gutil = require('gulp-util');
+var through = require('through2');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+module.exports = function() {
+
+
+	// create a stream through which each file will pass
+	return through.obj(function(file, enc, callback) {
+
+		if (file.isNull()) {
+			this.push(file);
+			// do nothing if no contents
+			return callback();
+		}
+
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError('gulp-img64', 'Streaming not supported'));
+			return callback();
+		}
+
+		if (file.isBuffer()) {
+			var $ = cheerio.load(String(file.contents));
+			$('img').each(function(){
+				if(this.attr('src') != ""){
+					var ssrc = this.attr('src');
+					var spath = path.join(file.base, ssrc);
+					var mtype = mime.lookup(spath);
+					console.log(mtype);
+					var sfile = fs.readFileSync(spath);
+					var simg64 = new Buffer(sfile).toString('base64');
+				};
+				this.attr('src','data:'+mtype+';base64,'+simg64);
+			});
+			var output = $.html();
+
+			file.contents = new Buffer(output);
+
+			return callback(null, file);
+		}
+	});
+}; 
---
+++ test/test.js
@@ -0,0 +1,33 @@
+var assert   = require('assert');
+var gutil    = require('gulp-util');
+var supersede = require('../index');
+var fs       = require('fs');
+var path     = require('path');
+var mime	= require('mime');
+
+describe('gulp-img64', function() {
+	describe('in buffer mode', function() {
+
+		it('should replace images in DOM with base64 data', function(done) {
+
+			var filename = path.join(__dirname, '/fixtures/input.html');
+
+			var input = new gutil.File({
+				base: path.dirname(filename),
+				path: filename,
+				contents: new Buffer(fs.readFileSync(filename, 'utf8'))
+			});
+
+			var stream = img64();
+
+			stream.on('data', function(newFile) {
+				assert.equal(String(newFile.contents), fs.readFileSync(path.join(__dirname, '/fixtures/output.html'), 'utf8'));
+				done();
+			});
+
+			stream.write(input);
+
+		});
+
+	});
+});
